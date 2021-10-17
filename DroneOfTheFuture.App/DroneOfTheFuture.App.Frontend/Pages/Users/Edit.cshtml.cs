36765ac2794e879d43577ee1d9e831b0599fb7e2
Geovanny Reyes <geovanny.reyes@correounivalle.edu.co>
2021-10-17 11:47:36 -0500

    creating fontend

---
+++ DroneOfTheFuture.App/DroneOfTheFuture.App.Frontend/Pages/Users/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HolaWeb.App.Persistencia.AppRepositorios;
+using HolaWeb.App.Dominio;
+
+namespace DroneOfTheFuture.App.Frontend.Pages
+{
+}
